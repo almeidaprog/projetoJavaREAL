2b1afcad485b70e0e63ac3aeabf9a66e90619712
Giovanna Almeida <aspectofstarlight@gmail.com>
2025-05-05 18:49:34 -0300

    Create DefaultSheet.java

---
+++ backend/backendRPG/src/main/java/com/rpgsheets/model/DefaultSheet.java
@@ -0,0 +1,15 @@
+package com.rpgsheets.model;
+
+public class DefaultSheet {
+
+    //atributos em comum
+    protected int forca;
+    protected int destreza;
+    protected int carisma;
+    protected int inteligencia;
+
+    //perícias em comum
+    protected int sobrevivencia;
+    protected int furtividade;
+    protected int intimidacao;
+}
